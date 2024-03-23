56b59c23451c303447187c3fe1412add28b24082
Anish Pankajkumar Patel <n01667320@humber.ca>
2024-03-22 20:54:59 -0400

    Add project files.

---
+++ Cumulative1/Controllers/TeacherController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cumulative1.Models;
+
+
+namespace Cumulative1.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: /Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        //GET : /Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+
+
+            return View(NewTeacher);
+        }
+
+
+    }
+}
